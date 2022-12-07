d5fbd22fc64a6383e4e0fa8afe121e1567c25d42
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2022-12-06 21:27:36 -0500

    feat: Initial home design

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
 
+  goTologin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  loginVerified(): boolean {
+    let accessToken = 'token';
+    if (accessToken) {
+      return true;
+    }
+    return false;
+  }
 }
